fcf0ab2738e40adfaf27048f5b77ce11a9da2fef
Tirta Rachman <tirtawening@gmail.com>
2020-06-26 20:13:24 -0400

    Update site location

---
+++ docs/build/precache-manifest.aa57b1885da4e63d51c641fd3c9cb428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c9295a34960b4f8acd02dc97f14e62",
+    "url": "/locanesia-integrated-maps/index.html"
+  },
+  {
+    "revision": "bafa7d3f0514bec8b1c8",
+    "url": "/locanesia-integrated-maps/static/css/main.54de3cc5.chunk.css"
+  },
+  {
+    "revision": "e981cf6a36e1cda582f2",
+    "url": "/locanesia-integrated-maps/static/js/2.72ab2384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/locanesia-integrated-maps/static/js/2.72ab2384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafa7d3f0514bec8b1c8",
+    "url": "/locanesia-integrated-maps/static/js/main.b3a0b8cd.chunk.js"
+  },
+  {
+    "revision": "464912dc76972c6c538a",
+    "url": "/locanesia-integrated-maps/static/js/runtime-main.d7fe874e.js"
+  }
+]);
